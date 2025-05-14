3f5aec739f3cb160f096e075b9f2830abbefe00e
Rik Cabanier <cabanier@meta.com>
2025-05-14 15:48:13 +0000

    address review comment

--- src/renderers/common/XRManager.js
+++ src/renderers/common/XRManager.js
@@ -1260,7 +1260,7 @@ function onSessionEnd() {
 
 	// restore framebuffer/rendering state
 
-	renderer.XRResetState();
+	renderer._resetXRState();
 
 	this._session = null;
 	this._xrRenderTarget = null;
